b04b44958f09a9f16b6cdae25dc1fa616c661c8b
Ram kumar <infogram0rk@gmail.com>
2022-09-18 18:09:32 +0530

    This is the commit for web application

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+
 
 app=Flask(__name__)
 ## load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -16,10 +19,27 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict((new_data))
     print(output[0])
     return jsonify(output[0])
 
-    if __name__=="__main__":
-        app.run(debug=True)
+if __name__=="__main__":
+    app.run(debug=True)
+"""{
+	"data": {
+		"CRIM": 0.07832,
+		"ZN": 31.0,
+		"INDUS": 1.31,
+		"CHAS": 0.0,
+		"NOX": 0.538,
+		"RM": 6.575,
+		"AGE": 65.2,
+		"DIS": 4.0900,
+		"RAD": 1.0,
+		"TAX": 296,
+		"PTRATIO": 15.3,
+		"B": 396.90,
+		"LSTAT": 4.98
+	}
+}"""
